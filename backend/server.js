e4e4dfefc4a536484e48000d0b9cea36514b5d8e
Hansika <hansikadonga@gmail.com>
2026-09-09 21:24:27 +0530

    backend setup and MySQL connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const db = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Smart Study Planner Backend is running!"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
